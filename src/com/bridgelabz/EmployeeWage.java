472f8912521e0f351517463511d07ea86fbe9e08
Rupali Anap <anaprupali333@gmail.com>
2022-12-24 10:39:39 -0800

    Added: Using Switch Case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -10,16 +10,18 @@ public class EmployeeWage {
 
         int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
 
-        if (employeeCheck == 2) {
-            DAILYWAGE = FULLTIMEHOUR * WAGEPERHOUR;
-            System.out.println("Employee is present full time");
-        } else if (employeeCheck == 1) {
-            DAILYWAGE = PARTTIMEHOUR * WAGEPERHOUR;
-            System.out.println("Employee is present half time");
-        } else {
-            System.out.println("Employee is absent");
+        switch (employeeCheck) {
+            case 2:
+                DAILYWAGE = FULLTIMEHOUR * WAGEPERHOUR;
+                System.out.println("Employee is present full time");
+                break;
+            case 1:
+                DAILYWAGE = PARTTIMEHOUR * WAGEPERHOUR;
+                System.out.println("Employee is present half time");
+                break;
+            default:
+                System.out.println("Employee is absent");
         }
         System.out.println("Employee total wage is: " +DAILYWAGE);
     }
-    }
-
+}
