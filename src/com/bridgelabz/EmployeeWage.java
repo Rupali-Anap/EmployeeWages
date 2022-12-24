7a0a1f372f61ae0b26d36e6738ad8ede19ae3861
Rupali-Anap <107263937+Rupali-Anap@users.noreply.github.com>
2022-12-24 10:17:02 -0800

    Update EmployeeWage.java

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,7 +3,6 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("**********WELCOME TO EMPLOYEE WAGES*************");
-       UC2_EmployeeDailyWages
         int WAGEPERHOUR = 20;
         int FULLDAYHOUR = 8;
         int DAILYWAGE = 0;
@@ -16,10 +15,9 @@ public class EmployeeWage {
             System.out.println("Employee is absent");
         }
         System.out.println("Employee total wage is: " +DAILYWAGE);
-=======
+
        }
-       master
-    }
+   
 }
 
 
